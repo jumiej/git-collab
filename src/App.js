80e71f12694c88dd5911434370db2acc5c97a052
jumiej <akinyodejummy177@gmail.com>
2023-05-17 13:15:12 +0100

    added my skeletal work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
 import Herosection from './hero-section/heroSection';
+import CardUp from "./components/CardUp";
 
 function App() {
-const testArray = [{name:'charles', age:10 ,eligible:true },{name:'mike',age:20, eligible: false}, {name:'justin', age: 34, eligible: true}]
-let checking;
-checking = testArray.filter((back)=>{ 
-})
-console.log(checking)
+// const testArray = [{name:'charles', age:10 ,eligible:true },{name:'mike',age:20, eligible: false}, {name:'justin', age: 34, eligible: true}]
+// let checking;
+// checking = testArray.filter((back)=>{ 
+// })
+// console.log(checking)
 
 
-  console.log('hello world')
+//   console.log('hello world')
   return (
     <div className="App">
-      <div>Hi Guys</div>
       <Herosection/>
+      <CardUp/>
     </div>
   );
 }
---
+++ src/Components/CardUp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import imag2 from "../images/imag2 (1) comp.png"
+
+function CardUp() {
+  return (
+    <>
+        <div>
+            <img src={imag2} alt=''/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Et voluptatibus nam sint voluptates ullam fugit eveniet, enim aperiam.
+             Suscipit tempora vel numquam temporibus doloremque magni ratione asperiores, 
+             rerum velit aliquam!
+             </p>
+        
+        </div>
+    </>
+  )
+}
+
+export default CardUp
