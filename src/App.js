79b6071ad9c5f4afc09d2df89c24099d97d1f320
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-05-17 16:26:58 +0100

    conflict resolved-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Herosection from "./hero-section/heroSection";
 import NavDown from "./Components/NavDown";
+import CardDown from "./card-down/card-down";
 
 function App() {
 
