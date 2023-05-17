eb9d046a4281c54e737d464817b93b5f305ede8d
chiemezie nduaguba <anthonynduaguba123@gmail.com>
2023-05-17 13:37:33 +0100

    added-images-modified-appjs

--- src/hero-section/heroSection.jsx
+++ src/hero-section/heroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './heroSection.css';
-
+import img1 from "../images/img1.png"
 
 
 export default function Herosection(){
@@ -8,9 +8,20 @@ export default function Herosection(){
     return(
         <>
         <div className ="jumie">
-            <p>
-                my own hero section. for git learning
-            </p>
+            <div className="jumie-1">
+            <img src={img1} alt= "perfume-1"/>
+            </div>
+            <div className="jumie-2">
+                <p>
+                    os presentamos algunas <br/>
+                    da las fotografias de <br/>
+                    bodegon y publicitaries<br/>
+                    relizadas para la<br/>
+                    web y las.
+                </p>
+                <hr />
+
+            </div>
         </div>
         </>
     )
