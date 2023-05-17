327cb43868803354f3b68363e68f3b2f0542b649
user <user@XVIII-DEMS.local>
2023-05-17 15:13:46 +0100

    Feat: created navdown component

--- src/hero-section/heroSection.jsx
+++ src/hero-section/heroSection.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import './heroSection.css';
+import "./heroSection.css";
 
-
-
-export default function Herosection(){
-
-    return(
-        <>
-        <div className ="jumie">
-            <p>
-                my own hero section. for git learning
-            </p>
-        </div>
-        </>
-    )
+export default function Herosection() {
+  return (
+    <>
+      <div className="jumie">
+        <p>my own hero section. for git learning</p>
+      </div>
+    </>
+  );
 }
